3e3c7cc91a70b2237631e79d17a69017a46deab0
mpizzzle <michael.770211@gmail.com>
2017-12-03 00:32:36 +0000

    puzzle 2 part 2 complete

--- puzzle2.py
+++ puzzle2.py
@@ -1,9 +1,13 @@
 with open('files/puzzle2.txt') as f:
     file = f.readlines()
 
-sum = 0
-for line in file:
-    my_list = [int(number) for number in line[:len(line) - 1].split('\t')]
-    sum += max(my_list) - min(my_list)
+sum_difference = 0
+sum_division = 0
 
-print sum
+for line in file:
+    my_list = sorted([int(number) for number in line[:len(line) - 1].split('\t')])
+    sum_difference += max(my_list) - min(my_list)
+    sum_division += sum([sum([j / i if i < j and j % i == 0 else 0 for j in my_list]) for i in my_list])
+
+print sum_difference
+print sum_division
