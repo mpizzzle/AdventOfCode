1844cfa13ea82722cb675197a04c201e282123db
mpizzzle <michael.770211@gmail.com>
2017-12-03 00:02:13 +0000

    puzzle 1 part 2 complete

--- puzzle1.py
+++ puzzle1.py
@@ -2,3 +2,4 @@ with open('files/puzzle1.txt') as f:
     file = f.read()[:len(f.read()) - 1]
 
 print sum([ord(file[i]) - 48 if file[i] == file[(i + 1) % len(file)] else 0 for i in range(len(file))])
+print sum([ord(file[i]) - 48 if file[i] == file[(i + (len(file) / 2)) % len(file)] else 0 for i in range(len(file))])
