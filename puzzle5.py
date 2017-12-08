0df41a85d0559a57a0597d71ab2cb83962500989
mpizzzle <michael.770211@gmail.com>
2017-12-08 11:20:01 +0000

    puzzle 5 complete

--- puzzle5.py
+++ puzzle5.py
@@ -5,11 +5,26 @@ with open('files/puzzle5.txt') as f:
 
 pointer = 0
 step_counter = 0
+instructions2 = list(instructions)
 
 while pointer < len(instructions):
 	temp = pointer
 	pointer += instructions[pointer]
 	instructions[temp] += 1
+	step_counter += 1
+
+print step_counter
+
+pointer = 0
+step_counter = 0
+
+while pointer < len(instructions2):
+	temp = pointer
+	pointer += instructions2[pointer]
+	if instructions2[temp] >= 3:
+		instructions2[temp] -= 1
+	else:
+		instructions2[temp] += 1
 	step_counter += 1
 
 print step_counter
