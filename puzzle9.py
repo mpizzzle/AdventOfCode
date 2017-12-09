0dc88cab14c50cf64b5abd4dda682e742e7b1502
mpizzzle <michael.770211@gmail.com>
2017-12-09 15:17:45 +0000

    puzzle 9 part 1 complete

---
+++ puzzle9.py
@@ -0,0 +1,31 @@
+from sets import Set
+
+with open('files/puzzle9.txt') as f:
+    stream = list(f.read())
+
+garbage = False
+skip = False
+depth = 0
+total = 0
+
+for char in stream:
+    if not garbage:
+        if char == '{':
+            depth += 1
+            total += depth
+        if char == '}':
+            depth -= 1
+        if char == '<':
+            garbage = True
+    else:
+        if not skip:
+            if char == '!':
+                skip = True
+                continue
+            if char == '>':
+                garbage = False
+        else:
+            skip = False
+
+print total
+print depth
