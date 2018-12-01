e90183eec796cf05e193097d775267817958693f
mpizzzle <michael.770211@gmail.com>
2018-12-01 21:21:32 +0000

    cleaning up solution

--- 2018/puzzle1.py
+++ 2018/puzzle1.py
@@ -3,16 +3,16 @@ from sets import Set
 with open('files/puzzle1.txt') as f:
     file = f.readlines()
 
-#frequency = sum([int(s[1:]) if s[0] == '+' else int(s[1:]) * -1 for s in file])
 frequency = 0
 frequencies = Set()
-frequency_not_found = True
-while frequency_not_found:
+frequency_found = False
+
+while not frequency_found:
     for s in file:
         frequencies.add(frequency)
-        frequency += int(s[1:]) if s[0] == '+' else int(s[1:]) * -1
+        frequency += int(s)
         if frequency in frequencies:
-    	    frequency_not_found = False
+            frequency_found = True
             break
 
     print frequency
