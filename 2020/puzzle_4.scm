c34b4664c98c6bfd99ef11008ddc4df63ba6732c
mpizzzle <m@michaelpercival.xyz>
2020-12-17 13:35:02 +0000

    minor refactoring

--- 2020/puzzle_4.scm
+++ 2020/puzzle_4.scm
@@ -1,12 +1,5 @@
 (load "read_lines.scm")
-
-(define (tokenize str chr)
-  (let ((in (memq chr (string->list str)))
-        (idx (lambda (s c) (- (string-length s) (length (memq c (string->list s)))))))
-    (if in
-      (cons (substring str 0 (idx str (car in)))
-            (tokenize (substring str (+ (idx str (car in)) 1) (string-length str)) chr))
-      (cons str '()))))
+(load "tokenize.scm")
 
 (define (right-fold passports)
   (if (not (null? passports))
